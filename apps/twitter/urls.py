1628371528b8a8e5a6de320dd7ccc831bc5936f9
oriolaguilar <aguilars99@gmail.com>
2020-03-17 17:56:22 +0100

    Added Bootstrap

--- apps/twitter/urls.py
+++ apps/twitter/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from apps.twitter import views
 
+# hello_world App URL Configuration
+# This file contains a list of URL patterns that correspond to various views functions.
+
 urlpatterns = [
+	path('', views.twitter, name='feed')
 ]
--- apps/twitter/views.py
+++ apps/twitter/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def twitter(request):
+    return render(request, 'feed.html', {})
+
