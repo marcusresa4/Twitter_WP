707455d6b7c4184e5b8f48650566d39b5552af41
Marc Resa Enrich <marcus@MacBook-Pro-de-Marc.local>
2020-03-18 14:29:28 +0100

    models.py nearly completed

--- apps/twitter/models.py
+++ apps/twitter/models.py
@@ -1,33 +1,38 @@
 from django.db import models
-from django.core.validators import MinValueValidator
-from django.core.validators import MaxValueValidator
 from django.contrib.auth import models as auth_models
 
 
 class TwitterUser(models.Model):
-    username=models.CharField(max_length=15)
-    following=models.PositiveIntegerField()
-    followers=models.PositiveIntegerField()
+    username = models.CharField(primary_key=True, max_length=15)
+    following = models.PositiveIntegerField()
+    followers = models.PositiveIntegerField()
+    tweet = models.ManyToManyField('Tweet', related_name= 'personal_tweets')
 
     def __str__(self):
         return self.username
 
 class Tweet(models.Model):
-
+    id_tweet = models.PositiveIntegerField(primary_key=True, default=123)
+    text = models.TextField(max_length=280,default="", help_text="280 characters max")
+    hashtag_in_tweet= models.ManyToManyField('Hashtag', related_name='hashtags_tweet')
     def __str__(self):
         return self
     
 class Hashtag(models.Model):
+    hashtag = models.TextField(primary_key=True, default="", max_length=279) # We have to keep in mind character "#"
 
     def __str__(self):
         return self
 
 class Statistics(models.Model):
-
+    type_stat = models.CharField(primary_key=True, default="", max_length=7) # Retweet is the longest (likes length 5)    
     def __str__(self):
         return self
 
 class Impact(models.Model):
+    id_tweet = models.ForeignKey('Tweet', default=123, on_delete=models.CASCADE)
+    type_stat = models.ForeignKey('Statistics',default="", on_delete=models.CASCADE)
+    stat_value = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self
