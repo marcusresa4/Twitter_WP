0fa9fee9db5dc2110090493288e0865f8a42b8b0
root <root@LAPTOP-K8D8G0S9.localdomain>
2020-03-25 00:00:12 +0100

    Changes Heroku 11

--- twitter_project/settings.py
+++ twitter_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -71,7 +71,7 @@ TEMPLATES = [
 ]
 
 
-WSGI_APPLICATION = 'twitter_project.wsgi.application'
+WSGI_APPLICATION = 'twitter_project.wsgi:application'
 
 
 # Database
@@ -79,10 +79,14 @@ WSGI_APPLICATION = 'twitter_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'dj_database_url.config(conn_max_age=600,ssl_require=True)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
