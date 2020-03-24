ab08dd44b062fd68f5165a3650f444d3298fb4a2
root <root@LAPTOP-K8D8G0S9.localdomain>
2020-03-24 23:02:09 +0100

    Changes Heroku 2

--- twitter_project/wsgi.py
+++ twitter_project/wsgi.py
@@ -8,8 +8,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-if path not in sys.path:
-    sys.path.append(path)
+
 from django.core.wsgi import get_wsgi_application
 from django.contrib.staticfiles.handlers import StaticFilesHandler
 
