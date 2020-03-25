b1583aa0ec6045f79d79a02717f54dfed1995a15
root <root@LAPTOP-K8D8G0S9.localdomain>
2020-03-25 21:25:50 +0100

    Changes heroku 23

--- twitter_project/settings.py
+++ twitter_project/settings.py
@@ -38,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.twitter',
-    'social_django',
+    'django_social',
     'apps.oauth2',
-    'social-django'
 
 ]
 
