25cab7bd9f2de22359f09bce1f4c24957a2078b3
Emili Resa vaque <resaenrich@iMac-de-Emili.local>
2020-04-30 13:00:15 +0200

    passa el primer step

--- features/steps/authentification.py
+++ features/steps/authentification.py
@@ -4,15 +4,19 @@ from django.contrib.auth.models import User
 
 use_step_matcher("parse")
 
-#No se si es pot fer ja que s'ha d'introduir un CAPTCHA
-@given('I login as user with Google {username} {name} {surname} {password}')
-def step_impl(context, username, name, surname, password):
-    c = Client()
-    user = User.objects.create_user(username=username, first_name=name, last_name=surname,
-                                    email='user@example.com', password=password)
-    c.force_login(user)
-
 @given('Exists a user logged {username} {name} {surname} {password}')
 def step_impl(context, username, name, surname, password):
-    User.objects.create_user(username=username, first_name=name, last_name=surname,
+    from django.contrib.auth.models import User
+    c = Client()
+    user = User.objects.create_user(username=username, first_name=name, last_name=surname,
                              email='user@example.com', password=password)
+    c.force_login(user)
+
+@given('I login as user with Google {username} {name} {surname} {password}')
+def step_impl(context, username, name, surname, password):
+    from django.contrib.auth.models import User
+
+    User.objects.create_user(username=username, first_name=name, last_name=surname,
+                                    email='user@example.com', password=password)
+
+
