d249958ad450a4df25384e7968c75f8f01ba02ae
Emili Resa vaque <resaenrich@iMac-de-Emili.local>
2020-05-01 21:15:19 +0200

    Loggout works fine and user can access to feed without logging in

--- apps/oauth2/views.py
+++ apps/oauth2/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.contrib.messages.storage import session
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
 
 def index(request):
- 	return render(request, 'index.html', {})
+    return render(request, 'index.html', {})
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'logout.html', {})
