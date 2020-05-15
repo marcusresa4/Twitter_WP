e4053ca85731f6c656dbea062c602b2ec8de5d1e
Marc Resa Enrich <marcresaenrich@MacBook-MacBook-Pro-de-Marc.local>
2020-05-15 10:15:54 +0200

    First commit new branch

--- twitter_project/settings.py
+++ twitter_project/settings.py
@@ -17,9 +17,6 @@ from decouple import config
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
